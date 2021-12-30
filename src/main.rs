f89563389875c4b144f181626042ec51923f9430
Jen Tak <jentak@hey.com>
2021-12-30 14:04:59 +0100

    Add mp3 examples and load them.
    - Use Surf Solar for it being the heaviest track in my opinion. ⬛️
    - Load and parse the mp3 into individual samples and concatenate them
      into one big vector. It's super slow, but we can optimize later.
    - Don't generate the matrix just yet. That would work out. We need to
      work over larger pieces than the individual samples.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-fn main() {}
+use std::io::{self};
 
-fn self_similate(me: Vec<i8>) -> Vec<Vec<bool>> {
+use minimp3::{Decoder, Error, Frame};
+
+fn main() {
+    let mut decoder = Decoder::new(io::stdin());
+    let mut frame_buf: Vec<Frame> = vec![];
+    loop {
+        match decoder.next_frame() {
+            Ok(frame) => frame_buf.push(frame),
+            Err(Error::Eof) => break,
+            Err(e) => panic!("{:?}", e),
+        }
+    }
+    println!("frames: {}", frame_buf.len());
+    let samples = frame_buf.iter().flat_map(|f| { f.data.clone() } ).collect::<Vec<_>>();
+    println!("samples: {}", samples.len());
+    // FIXME: this would allocate close to 20 TB when run with the surf_solar_short.mp3 sample.
+    // let matrix = self_similate(samples);
+    println!("done!");
+}
+
+fn self_similate(me: Vec<i16>) -> Vec<Vec<bool>> {
     me.iter()
         .map(|it| me.iter().map(|that| it == that).collect())
         .collect()
